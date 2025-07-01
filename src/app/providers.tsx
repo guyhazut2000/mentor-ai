e5e40ede4ec45f4c1fd0cb60918dbbeb2be68ed6
guy2342 <guyhazut2000@gmail.com>
2025-07-01 22:31:31 +0300

    add providers and fix middleware

---
+++ src/app/providers.tsx
@@ -0,0 +1,18 @@
+import { ClerkProvider, ClerkLoaded } from "@clerk/nextjs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <div>
+      <ClerkProvider>
+        <ClerkLoaded>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </ClerkLoaded>
+      </ClerkProvider>
+    </div>
+  );
+};
